05faee6f235316548aef791c22410ac3cacfe9d8
atomtick <li.liuwei@icloud.com>
2026-06-30 17:28:17 +0800

    add SCADA.ScriptSteps project

---
+++ SCADA.ScriptSteps/Step.cs
@@ -0,0 +1,9 @@
+﻿namespace SCADA.ScriptSteps
+{
+    public class Step
+    {
+        public bool IsLoopStart { get;set;  }
+        public bool IsLoopEnd { get;set; }
+        public int LoopCount {  get;set; }
+    }
+}
